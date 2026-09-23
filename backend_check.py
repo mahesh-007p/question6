f8dabe2c795eace70a1d48a45211c57e4082ce22
mahesh-007p <maheshgaming599@gmail.com>
2026-09-23 16:38:08 +0530

    fourth version

--- backend_check.py
+++ backend_check.py
@@ -1,10 +1,2 @@
-import time
-print("Running Backend")
 
-time.sleep(4)
-
-with open("backend_report.txt","w") as f:
-    f.write("Application Report\n")
-    f.write("Backend run\n")
-
-print("Backend report Generated")
+print("Backend report Running")
